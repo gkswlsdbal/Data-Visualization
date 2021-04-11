12a4e6dea2a278a96959875fad4916aa118e8703
yugin <74486425+buksil@users.noreply.github.com>
2021-04-11 22:42:37 +0900

    Add files via upload
    main의 init과 UI일부가 변경되었습니다.

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
 import main_event as ev
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import function
 import cellInfo
 import addTab
 
@@ -12,6 +16,9 @@ form_class = uic.loadUiType('ProjectUI.ui')[0]
 
 # 화면을 띄우는데 사용되는 Class 선언
 class WindowClass(QMainWindow, form_class):
+    fileCount = 0
+    currentData = ''
+    newData = ''
 
     def __init__(self):
         super().__init__()
@@ -22,6 +29,17 @@ class WindowClass(QMainWindow, form_class):
         self.FileList.itemDoubleClicked.connect(self.fileClick)
         self.actionCellAbsorption.triggered.connect(self.actionCells)
         self.actionFileAbsorption.triggered.connect(self.actionFiles)
+        self.menuSave.triggered.connect(self.actionSaves)
+        self.actionSave.triggered.connect(self.newSaves)
+        self.cellList.itemClicked.connect(self.cellClick)
+        self.fig = plt.figure()
+        self.fig.set_size_inches(5.5, 4)
+        self.canvas = FigureCanvas(self.fig)
+        self.graphLayout.addWidget(self.canvas)
+
+        path = '/Windows/Fonts/gulim.ttc'
+        font_name = fm.FontProperties(fname=path, size=50).get_name()
+        plt.rc('font', family=font_name)
         self.cellList.itemClicked.connect(self.drawCellInfo)
         self.addTabBtn.clicked.connect(self.drawCellInfo)
 
@@ -32,34 +50,38 @@ class WindowClass(QMainWindow, form_class):
         if object is self:
             ev.eventFilter(self, object, event)
         return False
-
     def btnClick(self):
         ev.btnClick(self)
 
     def fileClick(self):
+        self.colInfoListWidget.clear()
         ev.fileClick(self)
 
     def fileCheck(self, file):
         ev.fileCheck(self, file)
-
     def actionCells(self):
         ev.CellAbsorption(self)
-
     def actionFiles(self):
         ev.FileAbsorption(self)
 
+    def actionSaves(self):
+        ev.FileSave(self)
     def drawCellInfo(self):
         cellInfo.cellInfo(self)
 
+    def newSaves(self):
+        ev.newSave(self)
     def addTab(self):
         addTab.addTab(self)
 
+    def cellClick(self):
+        function.cellInfo(self)
+        function.cellClick(self)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyle(QtWidgets.QStyleFactory.create('Fusion'))
     myWindow = WindowClass()
-
     # 프로그램 화면을 보여주는 코드
     myWindow.show()
     app.exec_()
